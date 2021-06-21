ad19485f3659c7bc8a6b5e28e66a9c427c9bed57
Vitaki <nathan.mendes@hotmail.fr>
2021-06-21 16:31:05 +0200

    réparation quelques murs et préparation grab

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    public Animator KnobAnimator;
+    public bool InRange, NarrationActivationCheck, Grabbed;
+
+    public Narration.Narrations NarrationNeeded;
+    bool NarrationNeededCheck;
+
+    public enum LogoChoice
+    {
+        Hand,
+        Eye
+    }
+    public LogoChoice logo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (KnobAnimator != null)
+        {
+            if (InRange && NarrationActivationCheck)
+            {
+                KnobAnimator.SetBool("Ball", true);
+            }
+            else
+            {
+                KnobAnimator.SetBool("Ball", false);
+            }
+
+            if (logo == LogoChoice.Eye && looked)
+            {
+                KnobAnimator.SetBool("Eye", true);
+            }
+            else
+            {
+                KnobAnimator.SetBool("Eye", false);
+            }
+
+            if (logo == LogoChoice.Hand && looked)
+            {
+                KnobAnimator.SetBool("Hand", true);
+            }
+            else
+            {
+                KnobAnimator.SetBool("Hand", false);
+            }
+
+            if (interactable)
+            {
+                KnobAnimator.SetBool("Interactable", true);
+            }
+            else
+            {
+                KnobAnimator.SetBool("Interactable", false);
+            }
+
+            if (blocked)
+            {
+                KnobAnimator.SetBool("Locked", true);
+            }
+            else
+            {
+                KnobAnimator.SetBool("Locked", false);
+            }
+        }*/
+
+    }
+
+    public void Grab()
+    {
+
+    }
+
+    public void Release()
+    {
+
+    }
+}
